50622db04333d0b3279efe220eb363ad1fa67f52
Léo Bouzon <pp14aur@eduvaud.ch>
2026-01-14 15:05:05 +0100

    feat(commencement): test

---
+++ src/P-LeoBouzon-tower-defense-OO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P_LeoBouzon_tower_defense_OO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
