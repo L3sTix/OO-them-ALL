f4ded84d4dcb72b1a0d55230850b59f0dbde2307
L3sTrix <bouz1leo@outlook.com>
2026-01-28 15:49:36 +0100

    feat(v1-rendu): les différentes fonctionnalitées demandées pour le premier rendu sont fonctionnelles. PlacerTour, DeplacerEnnemis, TirerTour, CalculerDegats

--- src/P-LeoBouzon-tower-defense-OO/Program.cs
+++ src/P-LeoBouzon-tower-defense-OO/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Data.Common;
 using System.Threading;
 namespace P_LeoBouzon_tower_defense_OO
 {
@@ -8,23 +9,29 @@ namespace P_LeoBouzon_tower_defense_OO
 
         static void Main(string[] args)
         {
-            int[] tourPlace = new int[20];
-            int towerPosition = tourPlace[0];
-            int[] cheminDeJeu = new int[20];
-            int maxMoveX = 19;
-            int towerMouvementX = 1;
-            int minMoveX = 0;
-            int oldTowerPosition;
-            int toursPlacees = 0;
-            int maxTours = 4;
-            int ennemiPV = 100;
-            int ennemiPosition = 0;
-            int oldEnnemiPosition;
-            int ennemiMouvementX = 1;
+            // ----- VARIABLES -----
+
+            int[] tourPlace = new int[20];                                          // Tableau contenant les tours
+            int towerPosition = tourPlace[0];                                       // Initialisation du curseur ou placer des tours
+            int[] cheminDeJeu = new int[20];                                        // tableau du 
+            int maxMoveX = 19;                                                      // Limite droit du tableau
+            int towerMouvementX = 1;                                                // Mouvement horizontal d'une tour
+            int minMoveX = 1;                                                       // Limite gauche du tableau
+            int oldTowerPosition;                                                   // Ancienne position de tour
+            int toursPlacees = 0;                                                   // Nombre de tours placées
+            int maxTours = 4;                                                       // Maximum de tours pouvant être placée
+            int degats = 25;                                                        // Nombre de dégats infligés à l'ennemi par tire
+            int ennemiPvInitial = 100;                                              // PV max de l'ennemi lorsqu'il apparaît
+            int ennemiPV = ennemiPvInitial;                                         // PV de l'ennemi au fil du jeu 
+            int ennemiPosition = 0;                                                 // Position d'un ennemi
+            int oldEnnemiPosition;                                                  // Ancienne position d'un ennemi
+            int ennemiMouvementX = 1;                                               // Mouvement horizontal d'un ennemi
+            int chemin = 0;                                                         // Initialisation du chemin en "═"
             MenuDeLancement();
             PlacerTour();
             DeplacerEnnemis();
             Console.ReadLine();
+            // ----- MENU DE JEU ET MENU POSSIBLE INTERACTIF -----
             static void MenuDeLancement()
             {
                 Console.WriteLine("--------------------------------");
@@ -39,6 +46,7 @@ namespace P_LeoBouzon_tower_defense_OO
                 Console.ReadKey();
                 Console.Clear();
             }
+            // ----- PLACEMENT DES TOURS PAR L'UTILISATEUR -----
             void PlacerTour()
             {
                 ConsoleKeyInfo towerMove;
@@ -97,45 +105,103 @@ namespace P_LeoBouzon_tower_defense_OO
                     Console.Write("T");     
                 }
             }
+            // ----- DEPLACEMENT DES ENNEMIS SUR LE LONG DU CHEMIN -----
             void DeplacerEnnemis()
             {
                 Console.CursorVisible = false;
-                Console.SetCursorPosition(ennemiPosition, 2);
+                
+                for (int i = 0; i < cheminDeJeu.Length + 1; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.SetCursorPosition(chemin, 2);
+                    Console.Write("═");
+                    Console.SetCursorPosition(chemin, 4);
+                    Console.Write("═");
+                    chemin += 1;
+                    Console.ResetColor();
+
+                }
+                Console.SetCursorPosition(20, 3);
+                Console.Write("C");
+                Console.SetCursorPosition(ennemiPosition, 3);
                 Console.Write("E");
+                Console.SetCursorPosition(0, 5);
+                Console.Write($"PV restants : {ennemiPvInitial} ");
                 do
                 {
-                    oldEnnemiPosition = ennemiPosition;
-
-                    if (ennemiPosition < cheminDeJeu.Length)
+                    if (ennemiPV <= 0)
                     {
-                        ennemiPosition += ennemiMouvementX;
+                        
+                        ennemiPosition = cheminDeJeu.Length;
+                        Console.SetCursorPosition(0, 6);
+                        Console.Write($"Ennemi mort ");
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Console.CursorVisible = false;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.SetCursorPosition(30, 10);
+                        Console.WriteLine("###############################################################");
+                        Console.SetCursorPosition(30, 11);
+                        Console.WriteLine("#                                                             #");
+                        Console.SetCursorPosition(30, 12);
+                        Console.WriteLine("#   ██╗   ██╗  ██████╗ ██╗   ██╗  ██╗    ██╗ ██╗ ███╗   ██╗   #");
+                        Console.SetCursorPosition(30, 13);
+                        Console.WriteLine("#   ╚██╗ ██╔╝ ██╔═══██╗██║   ██║  ██║    ██║ ██║ ████╗  ██║   #");
+                        Console.SetCursorPosition(30, 14);
+                        Console.WriteLine("#    ╚████╔╝  ██║   ██║██║   ██║  ██║ █╗ ██║ ██║ ██╔██╗ ██║   #");
+                        Console.SetCursorPosition(30, 15);
+                        Console.WriteLine("#      ██╔╝   ██║   ██║██║   ██║  ██║███╗██║ ██║ ██║╚██╗██║   #");
+                        Console.SetCursorPosition(30, 16);
+                        Console.WriteLine("#      ██║    ╚██████╔╝╚██████╔╝  ╚███╔███╔╝ ██║ ██║ ╚████║   #");
+                        Console.SetCursorPosition(30, 17);
+                        Console.WriteLine("#      ╚═╝     ╚═════╝  ╚═════╝    ╚══╝╚══╝  ╚═╝ ╚═╝  ╚═══╝   #");
+                        Console.SetCursorPosition(30, 18);
+                        Console.WriteLine("#                                                             #");
+                        Console.SetCursorPosition(30, 19);
+                        Console.WriteLine("###############################################################");
+                        Console.ResetColor();
+
                     }
                     else
                     {
-                        ennemiPosition = ennemiMouvementX;
+                        oldEnnemiPosition = ennemiPosition;
+
+                        if (ennemiPosition < cheminDeJeu.Length)
+                        {
+                            ennemiPosition += ennemiMouvementX;
+                        }
+                        else
+                        {
+                            ennemiPosition = ennemiMouvementX;
+                        }
+
+                        Console.SetCursorPosition(oldEnnemiPosition, 3);
+                        Console.Write(" ");
+
+                        Console.SetCursorPosition(ennemiPosition, 3);
+                        Console.Write("E");
+
+                        TirerTour();
+                        if (ennemiPV <= 0)
+                        {
+                            Console.SetCursorPosition(ennemiPosition, 3);
+                            Console.Write("X");
+                        }
+                        Thread.Sleep(1000);
                     }
-
-                    Console.SetCursorPosition(oldEnnemiPosition, 2);
-                    Console.Write(" ");
-
-                    Console.SetCursorPosition(ennemiPosition, 2);
-                    Console.Write("E");
-
-                    TirerTour();
-
-                    Thread.Sleep(1000);
-                } while (ennemiPosition != cheminDeJeu.Length);
-                if (ennemiPV <= 0)
-                {
-                    Console.SetCursorPosition(0, 6);
-                    Console.WriteLine("YOU WIN !");
-                }
+                    
+                } while (ennemiPosition != cheminDeJeu.Length );
+                
             }
-            
+            // ----- TIRE DES TOURS SUR LES ENNEMIS -----
             void TirerTour()
             {
-                if (ennemiPV <= 0);
                 bool ennemiTouche = false;
+                if (ennemiPV <= 0)
+                {
+                    ennemiTouche = false;
+                }
+                    
 
                 for (int i = 0; i < tourPlace.Length; i++)
                 {
@@ -143,24 +209,32 @@ namespace P_LeoBouzon_tower_defense_OO
                     {
                         if ((i - 1 >= 0 && ennemiPosition >= i - 1) || (ennemiPosition == i) || (i + 1 < tourPlace.Length && ennemiPosition == i + 1 ))
                         {
+                            
                             CalculerDegats();
                             ennemiTouche = true;
+                            
                         }
                     }
                 }
             }
+            // ----- CALCULE DES DEGATS INFLIGES  -----
             void CalculerDegats()
             {
-                int degats = 25;
-                ennemiPV -= degats;
+                                                                                
+                ennemiPV -= degats;                                             // le nombre de PV de l'ennemi diminue de 25 quand il est touché
 
-                if (ennemiPV < 0) ennemiPV = 0;
-
-                Console.SetCursorPosition(0, 4);
+                if (ennemiPV < 0)
+                {
+                    ennemiPV = 0;
+                    
+                }
+                Console.SetCursorPosition(0, 5);
+                Console.Write($"PV restants : {ennemiPV} ");
+                Console.SetCursorPosition(0, 6);
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.Write($"L'ennemi est touché (-" + degats + " PV) ");
                 Console.ResetColor();
-                Console.Write($"PV restants : {ennemiPV} ");
+                
             }
         }
     }
